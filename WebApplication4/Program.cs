1f872cc1b7d57b43ce7909f102e40798cf8bfb38
Nishu Goel <nishu.goel@nagarro.com>
2021-03-02 10:44:09 +0530

    First commit in develop branch

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication4
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Hello from develop branch");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
