45ed9bdb80155b2c90560e8e1404b6c54d31f815
Nishu Goel <nishu.goel@nagarro.com>
2021-03-02 10:42:08 +0530

    first commit to feature branch

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -14,6 +14,7 @@ namespace WebApplication4
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Hello from feature branch");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
